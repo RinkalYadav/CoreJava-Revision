673f6e342ee2d65f869a84b854a412f35e3b5d13
RinkalYadav <rinkalbyadav@gmail.com>
2026-03-19 11:01:48 +0530

    ...

--- JavaPractice/src/com/basic/com/PrintAlphabate.java
+++
@@ -1,9 +0,0 @@
-package com.basic.com;
-
-public class PrintAlphabate {
-public static void main(String[] args) {
-	for(char i='A'; i<='Z'; i++) {
-		System.out.print(i+" ");
-	}
-}
-}
--- JavaPractice/src/com/basic/com/StudentMarks.java
+++
@@ -1,8 +0,0 @@
-package com.basic.com;
-import java.util.Scanner;
-public class StudentMarks {
-public static void main(String[] args) {
-	Scanner sc=new Scanner(System.in);
-	System.out.println("Ennter the marks of ");
-}
-}
