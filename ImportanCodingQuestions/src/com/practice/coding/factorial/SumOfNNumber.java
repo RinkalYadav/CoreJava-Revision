fce20eee1d2ad721461839d2acd778de62572d7f
RinkalYadav <rinkalbyadav@gmail.com>
2026-03-20 12:39:30 +0530

    print Natural Number using recursion

---
+++ ImportanCodingQuestions/src/com/practice/coding/factorial/SumOfNNumber.java
@@ -0,0 +1,14 @@
+package com.practice.coding.factorial;
+
+public class SumOfNNumber {
+
+	static void print(int n) {
+		if(n==0) return;
+		
+		print(n-1);
+		System.out.println(n+" ");
+	}
+	public static void main(String[] args) {
+		print(5);
+	}
+}
